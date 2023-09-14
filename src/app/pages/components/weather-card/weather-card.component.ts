50993a3ae310627fbaf1f0bcddcf926c33ea8453
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2023-09-14 09:33:44 -0300

    projeto pronto

---
+++ src/app/pages/components/weather-card/weather-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from 'src/app/interfaces/weather.interface';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent {
+  @Input()
+  weatherDatasInput!: WeatherDatas;
+  // Dados da previsao do tempo recebidos pelo componente pai
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+}
